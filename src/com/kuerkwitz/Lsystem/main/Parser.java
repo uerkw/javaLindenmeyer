f94d639bd8f5523dcddfe5b20aaab0662f671e4f
KyleUerkwitz <kyleuerkwitz@gmail.com>
2018-11-02 16:07:40 -0400

    Create empty file parser and test input data

---
+++ src/com/kuerkwitz/Lsystem/main/Parser.java
@@ -0,0 +1,20 @@
+package com.kuerkwitz.Lsystem.main;
+
+import java.io.*;
+
+public class Parser {
+    File desiredFile;
+    String inputVars;
+    String inputConstants;
+    String inputStart;
+    String[] inputRules;
+    int[] inputAngles;
+    int inputIterations;
+
+    public Parser(File inputFile){
+        this.desiredFile = inputFile;
+    }
+    public void readFile(){
+
+    }
+}
