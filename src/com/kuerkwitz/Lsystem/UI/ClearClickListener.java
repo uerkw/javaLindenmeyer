d6abdb81fb0a5baabf6a193f7be02c3bc2766ee1
KyleUerkwitz <kyleuerkwitz@gmail.com>
2018-11-12 15:31:52 -0500

    Add listener to for clearing canvas.

---
+++ src/com/kuerkwitz/Lsystem/UI/ClearClickListener.java
@@ -0,0 +1,23 @@
+package com.kuerkwitz.Lsystem.UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClearClickListener implements ActionListener {
+    DisplayGraphics canvas;
+    public ClearClickListener(DisplayGraphics g){
+        this.canvas = g;
+    }
+    public void actionPerformed(ActionEvent e) {
+        Project2GUI.drawingLines.clear();
+        Project2GUI.drawingColors.clear();
+        canvas.repaint();
+
+        /*JOptionPane.showMessageDialog(null,
+                "Canvas has been cleared.",
+                "Clear Button Clicked",
+                JOptionPane.WARNING_MESSAGE); */
+    }
+}
